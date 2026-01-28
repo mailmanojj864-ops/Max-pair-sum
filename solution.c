48b79122740b0efd72c106296cae04abd620c1e9
Manoj <mailmanojj864@gmail.com>
2026-01-28 21:39:10 +0530

    Add files via upload

---
+++ solution.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+        int a[n];
+
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+        }
+        int firstmax = INT_MIN;
+        int secondmax = INT_MIN;
+        
+        for(int i = 0; i < n; i++){
+            if(a[i]>firstmax){
+                secondmax = firstmax;
+                firstmax = a[i];
+            }else if (a[i] > secondmax && a[i]!=firstmax){
+                secondmax = a[i];
+            }
+        }
+        int maxsum=firstmax+secondmax;
+        printf("%d\n",maxsum);
+        
+    }
+
+    return 0;
+}
+
+
